7b2467bb9b40f125c3034ee8d855c5afdc48461d
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-03-20 00:44:31 +0700

    Update install-menu.sh

--- scripts/setup/install-menu.sh
+++ scripts/setup/install-menu.sh
@@ -11,14 +11,11 @@ echo "Downloading and installing menu scripts..."
 wget -qO /usr/local/sbin/menu https://raw.githubusercontent.com/hambosto/TrojanVPN/main/scripts/menu/menu.sh && chmod +x /usr/local/sbin/menu
 wget -qO /usr/local/sbin/menu-vmess https://raw.githubusercontent.com/hambosto/TrojanVPN/main/scripts/menu/vmess.py && chmod +x /usr/local/sbin/menu-vmess
 wget -qO /usr/local/sbin/menu-vless https://raw.githubusercontent.com/hambosto/TrojanVPN/main/scripts/menu/vless.sh && chmod +x /usr/local/sbin/menu-vless
-echo "Menu scripts installed."
-
 # Download and install utility scripts
 echo "Downloading and installing utility scripts..."
 wget -qO /usr/local/sbin/cleaner https://raw.githubusercontent.com/hambosto/TrojanVPN/main/tools/cleaner.py && chmod +x /usr/local/sbin/cleaner
 wget -qO /usr/local/sbin/bbr https://raw.githubusercontent.com/hambosto/TrojanVPN/main/tools/bbr.sh && chmod +x /usr/local/sbin/bbr
 wget -qO /usr/local/sbin/expiry https://raw.githubusercontent.com/hambosto/TrojanVPN/main/tools/expiry.py && chmod +x /usr/local/sbin/expiry
 wget -qO /usr/local/sbin/speedtest-cli https://raw.githubusercontent.com/hambosto/TrojanVPN/main/tools/speedtest-cli.py && chmod +x /usr/local/sbin/speedtest-cli
-echo "Utility scripts installed."
 
 echo "Setup completed successfully."
